89396294ad7a221b1049c73c157f7b00a0dbd7bd
Vitor Miguel <38699921+VitorMiguelM@users.noreply.github.com>
2019-05-20 17:20:21 -0300

    Importando e exportando Jars

---
+++ Atividades/32ImportarJar/src/Principal.java
@@ -0,0 +1,12 @@
+import arquivo.Texto;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		
+		//Trabalhando com a classe Texto
+		Texto t = new Texto("Proway");
+		t.contarLetras();
+	}
+}
+
